5371043aa920ee7d59b65223561b6902f16dc076
giaozi <49359843+griffinchozick@users.noreply.github.com>
2022-02-19 17:46:07 -0500

    Renamed variables

--- processing.py
+++ processing.py
@@ -19,14 +19,14 @@ from_date = formatDate(datetime.datetime.now() - datetime.timedelta(days=1))
 
 response = requests.get(f"https://api.govinfo.gov/published/{from_date}/{to_date}?offset=0&pageSize=100&collection=BILLS&api_key={api_key}")
 
-bills = response.json()["packages"]
+packages = response.json()["packages"]
 
 bill_list = []
 #List of Bill Objects
 
-for bill in bills:
+for package in packages:
 	bill_committees = []
-	link = bill["packageLink"]
+	link = package["packageLink"]
 	info = requests.get(f"{link}?api_key={api_key}")
 	for committee in info.json()["committees"]:
 		bill_committees.append(committee["committeeName"])
